5e51b8c023dfdb79cfb9d9b93046342bd07d6de2
Christoph <fitzzz@fitzzz.de>
2017-12-13 13:13:41 +0100

    ...

---
+++ Warthog/Classes/CalendarEvent.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warthog.Classes
+{
+    public class CalendarEvent
+    {
+        public string Eventname { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime EventDate { get; set; }
+        public IList<IUser> Attendees { get; set; }
+        public IUser EventCreator { get; set; }
+    }
+}
